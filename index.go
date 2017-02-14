b733de7a7a581b68576746bc7de0f5c60bc2583a
Timothy Lo <tim7643@gmail.com>
2017-02-14 14:43:44 -0800

    added in alorithms for calculating neighbors and determining life

--- index.go
+++ index.go
@@ -68,22 +68,75 @@ func getsize(w http.ResponseWriter, r *http.Request) {
 func runlife(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
 	fmt.Println("running life \n")
-	// fmt.Println("world: \n", world)
-
 	// logic to populate neighbors
 	for i := 0; i < len(world); i++ {
 		for j :=0; j < len(world[i]); j++ {
-			// fmt.Println(world[i][j])
 			var count = 0
-			
+			if i>0 && j > 0 {
+				if world[i-1][j-1] == 1 {
+					count++
+				}
+			}
+			if i>0 {
+				if world[i-1][j] == 1 {
+					count++
+				}
+			}
+			if j > 0 {
+				if world[i][j-1] == 1 {
+					count++
+				}
+			}
+			if i > 0 && j < len(world[i])-1 {
+				if world[i-1][j+1] == 1 {
+					count++
+				}
+			}
+			if i < len(world[i])-1 && j > 0 {
+				if world[i+1][j-1] == 1 {
+					count++
+				}
+			}
+			if j < len(world[i])-1 {
+				if world[i][j+1] == 1 {
+					count++
+				}
+			}
+			if i < len(world[i])-1 {
+				if world[i+1][j] == 1 {
+					count++
+				}
+			}
+			if i < len(world[i])-1 && j < len(world[i])-1 {
+				if world[i+1][j+1] == 1 {
+					count++
+				}
+			}
 			neighbors[i][j] = count
 		}
-	  fmt.Println(world[i])
+		fmt.Println(world[i])
 	}
 	fmt.Println("neighbors: \n", neighbors)
-
+	// go through neighbors and reset world
+	for i := 0; i < len(world); i++ {
+		for j :=0; j < len(world[i]); j++ {
+			// Any live cell with fewer than two live neighbours dies, as if caused by underpopulation.
+			if world[i][j] == 1 && neighbors[i][j] < 2 {
+				world[i][j] = 0
+			}
+			// Any live cell with two or three live neighbours lives on to the next generation.
+			// Any live cell with more than three live neighbours dies, as if by overpopulation.
+			if world[i][j] == 1 && neighbors[i][j] > 3 {
+				world[i][j] = 0
+			}
+			// Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+			if world[i][j] == 0 && neighbors[i][j] == 3 {
+				world[i][j] = 1
+			}
+		}
+	}
 	t, _ := template.ParseFiles("conway.gtpl")
-	t.Execute(w, neighbors)
+	t.Execute(w, world)
 }
 
 func main() {
