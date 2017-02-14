9d9fe6dcbdf1ac7784d168a9fe39844b64e3a7b1
Timothy Lo <tim7643@gmail.com>
2017-02-14 04:41:53 -0800

    created multi=dimensional array

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ Learn and apply something new in a constrained time period. Take no longer than 
 Write Conway's Game of Life in a language you are not familiar. The final solution will be executable in some format and, ideally, will include automated unit tests.
 
 The languages you can choose from are Elm, Rust, Elixir, Clojure, Swift, or Go. Similar with the last challenge, this is not a about the finish line, its about the process, research, approach, and quality of the final code.
+
+https://en.wikipedia.org/wiki/Conway's_Game_of_Life
---
+++ index.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+"fmt"
+"math/rand"
+"time"
+)
+
+// create array of size "size" with random 0 or 1
+func createSet(size int) []int {
+	arr := make([]int, size)
+	for i := 0; i < size; i++ {
+		rand.Seed(int64(time.Now().Nanosecond()))
+		arr[i] = rand.Intn(2)
+	}
+	return arr
+}
+
+// creates multi-dimensional array of size "size"
+func createBoard(size int) [][]int {
+	board := make([][]int, size)
+	for i := 0; i < size ; i++ {
+		board[i] = createSet(size)
+	}
+	return board
+}
+
+func main() {
+	// sets size of world
+	var size int = 39
+
+	// create world
+	world := createBoard(size)
+
+	// shows world
+	fmt.Println(world)
+}
