96ffa27ca78b8361b7d25fc8f0e516c25dfc8f83
Timothy Lo <tim7643@gmail.com>
2017-02-14 09:42:37 -0800

    take user input and create multi-dim arrays

---
+++ getsize.gtpl
@@ -0,0 +1,11 @@
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<form action="/getsize" method="post">
+		How big should the world be:<input type="number" name="size">
+		<input type="submit" value="Size">
+	</form>
+</body>
+</html>
--- index.go
+++ index.go
@@ -4,6 +4,10 @@ import (
 "fmt"
 "math/rand"
 "time"
+"html/template"
+"log"
+"net/http"
+"strconv"
 )
 
 // create array of size "size" with random 0 or 1
@@ -25,18 +29,34 @@ func createBoard(size int) [][]int {
 	return board
 }
 
-func main() {
-
-	fmt.Print("Size of world: ")
-	var input int
-	fmt.Scanln(&input)
-
-	var world = createBoard(input)
-	var neighbors = make([][]int, input)
-
-	fmt.Print(world)
-	fmt.Print("\n")
-	fmt.Print(neighbors)
-	fmt.Print("\n")
-
+// get the size from the user
+func getsize(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("method:", r.Method)
+	if r.Method == "GET" {
+		t, _ := template.ParseFiles("getsize.gtpl")
+		t.Execute(w, nil)
+	} else {
+		r.ParseForm()
+		// changing slice to string
+		var stringSize string = r.FormValue("size")
+		// changing string to integer
+		intSized, err := strconv.Atoi(stringSize)
+		if err != nil {
+			log.Fatal(err)
+		}
+		// create world and neighbors
+		var world = createBoard(intSized)
+		fmt.Println("world: \n", world)
+		var neighbors = make([][]int, intSized)
+		fmt.Println("neighbors: \n", neighbors)
+	}
 }
+
+func main() {
+	http.HandleFunc("/", getsize)
+	err := http.ListenAndServe(":9090", nil) // setting listening port
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
+
